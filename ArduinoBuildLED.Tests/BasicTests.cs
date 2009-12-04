a50dded9b1f5168b308e98bf10e8ee9b8a744259
Matt Heye <matt.heye@gmail.com>
2009-12-04 06:30:42 +0000

    added unit tests

---
+++ ArduinoBuildLED.Tests/BasicTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ArduinoBuildLED.Tests
+{
+    [TestFixture]
+    public class BasicTests
+    {
+        [Test]
+        public void AdditionTest()
+        {
+            int i = 1 + 1;
+            Assert.IsTrue(i == 2);
+        }
+
+        [Test]
+        public void AdditionTest2()
+        {
+            int i = 1 + 1;
+            Assert.IsTrue(i == 3);
+        }
+
+        [Test]
+        public void AdditionTest3()
+        {
+            int i = 1 + 2;
+            Assert.IsTrue(i == 3);
+        }
+    }
+}
